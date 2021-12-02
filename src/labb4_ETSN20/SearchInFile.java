a786cd708273f976211ffcaedf09920999f66644
GirleyPower <zsabah.1996@gmail.com>
2021-12-02 10:28:23 +0100

    added something

--- src/labb4_ETSN20/SearchInFile.java
+++ src/labb4_ETSN20/SearchInFile.java
@@ -10,23 +10,24 @@ public class SearchInFile {
 		
 		SearchInFile search = new SearchInFile();
 		search.searchLines(args[0], args[1]);
-		
+
 	}
-	
+
 	public void searchLines(String pattern, String fileName) throws FileNotFoundException{
 
-		
+
 		//läs filen
 		File file = new File ("fileName");
 		Scanner scan = new Scanner(file); //läs från filen(om filen inte finns, throw exception)
-		
-		while (scan.hasNextLine()) { 
-			String line = scan.nextLine();					
+
+		while (scan.hasNextLine()) {
+			String line = scan.nextLine();
 			if(line.contains(pattern)) {
 				System.out.println("Pattern found at line: " + line);
 			}
 		}
-		
+		//adding comment
+
 		scan.close();
 	}
 
